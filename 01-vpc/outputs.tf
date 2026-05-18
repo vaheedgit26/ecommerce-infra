eaeca9ffaced67b0d1ac1efc36537af2cf442e44
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-18 14:14:13 +0530

    Update outputs.tf

--- 01-vpc/outputs.tf
+++ 01-vpc/outputs.tf
@@ -1,7 +1,23 @@
-output "vpc_id" { value = module.vpc.vpc_id }
+output "project" { value = module.vpc.project }
+output "env"     { value = module.vpc.env }
 
-output "public_subnet_ids"   { value = module.vpc.private_subnet_ids  }
-output "private_subnet_ids"  { value = module.vpc.public_subnet_ids   }
-output "database_subnet_ids" { value = module.vpc.database_subnet_ids }
+output "vpc_id"             { value = module.vpc.vpc.id   }
+output "vpc_cidr"           { value = module.vpc.vpc_cidr }
+output "availability_zones" { value = module.vpc.azs      }
+
+output "public_subnet_cidr"   { value = module.vpc.public_subnet_cidr   }
+output "private_subnet_cidr"  { value = module.vpc.private_subnet_cidr  }
+output "database_subnet_cidr" { value = module.vpc.database_subnet_cidr }
+
+output "public_subnet_ids"   { value = module.vpc.public[*].id  }
+output "private_subnet_ids"  { value = module.vpc.private.*.id  }
+output "database_subnet_ids" { value = module.vpc.database.*.id }
+
+output "public_route_table_id"   { value = module.vpc.public.id   }
+output "private_route_table_id"  { value = module.vpc.private.id  }
+output "database_route_table_id" { value = module.vpc.database.id }
+
+# This returns entire 'internet_gateway' object, if you want only 'id' then use "aws_internet_gateway.internet_gateway.id"
+output "internet_gateway" { value = module.vpc.internet_gateway }
   
  
