42c93794b9b864466cda791c9a7aab570b08a3f5
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-18 13:40:43 +0530

    Update locals.tf

--- 01-vpc/locals.tf
+++ 01-vpc/locals.tf
@@ -1,12 +1,14 @@
 locals {
   common_tags = {
-    Project     = var.project_name
+    Project     = var.project
     Environment = var.env
     Terraform   = "true"
   }
 
+  # eks cluster = ecommerce-dev-eks-cluster
+
   vpc_eks_tags = {
     "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/${var.eks_cluster_name}" = "owned"
+    "kubernetes.io/cluster/${var.project-var.env}-eks-cluster" = "owned"
   }
 }
