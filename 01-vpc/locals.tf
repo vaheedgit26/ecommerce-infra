9964b7ada97757568afee2bfcb527be781f9bc59
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-18 13:35:02 +0530

    Update locals.tf

--- 01-vpc/locals.tf
+++ 01-vpc/locals.tf
@@ -4,4 +4,9 @@ locals {
     Environment = var.env
     Terraform   = "true"
   }
+
+  vpc_eks_tags = {
+    "kubernetes.io/role/elb" = "1"
+     "kubernetes.io/cluster/${var.eks_cluster_name}" = "owned"
+  }
 }
