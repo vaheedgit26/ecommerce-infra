c1dbdcba8b8f29d402dc7e94f348fcda6038c778
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-20 22:31:19 +0530

    Update main.tf

--- 03-nat-instance/main.tf
+++ 03-nat-instance/main.tf
@@ -1,6 +1,6 @@
 # NAT-INSTANCE Module Calling
 module "nat_instance" {
-  source = "../../../modules/nat-instance"
+  source = "git::https://github.com/vaheedgit26/Infra-1.0//modules/nat-instance"
   # depends_on = [module.vpc]
 
   vpc_id                                  = module.vpc.vpc_id
