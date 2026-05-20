6556b96f28d87f399ebaece87178dcac4460e686
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-20 22:11:34 +0530

    Update provider.tf

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  # required_version = ">= 1.9.0"
+  #required_version = ">= 1.9.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,13 +8,7 @@ terraform {
   }
 
 # Remote Backend
- backend "s3" {
-    bucket  = "remote-state-aws-88s-dev" # Replace with your unique bucket name
-    key     = "terraform-eks-vpc"
-    region  = "us-east-1"
-    encrypt = true
-    use_lockfile   = true
-  }
+  backend "s3" {}      
 }
 
 provider "aws" {
