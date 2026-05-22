0037437ae7a69b40c48c670f32fd8796fbddac4c
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 23:04:22 +0530

    Update main.tf

--- 20-rds/main.tf
+++ 20-rds/main.tf
@@ -2,7 +2,7 @@ module "rds" {
   source = "git::https://github.com/vaheedgit26/Infra-1.0.git//modules/rds"
   database_subnet_ids = data.terraform_remote_state.vpc.outputs.database_subnet_ids       # For subnet group creation
 
-  identifier              = "${var.project}-${var.env}-postgres"
+  identifier              = "${var.project}-${var.env}-postgre"
   availability_zone       = data.terraform_remote_state.vpc.outputs.availability_zones[0]         # var.availability_zone
   engine                  = "postgres"
   engine_version          = "15.7"
