a8b6941b231b5a16ae1ac8bdbab8609d9547fe1b
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 22:48:00 +0530

    Update outputs.tf

--- 02-bastion/outputs.tf
+++ 02-bastion/outputs.tf
@@ -2,6 +2,6 @@ output "bastion_public_ip" {
   value = module.bastion_ec2.network.public_ip
 }
 
-output "bastion_security_group_ids" {
-  value = module.bastion_ec2.security_group_ids
+output "bastion_security_group_id" {
+  value = module.bastion_sg.sg_id
 }
