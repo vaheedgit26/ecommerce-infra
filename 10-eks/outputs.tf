2b4aa862408e9da7527cca745c79eb381be80e92
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 22:34:07 +0530

    Update outputs.tf

--- 10-eks/outputs.tf
+++ 10-eks/outputs.tf
@@ -1,36 +1,36 @@
 output "cluster_name" {
   description = "Name of the EKS cluster"
-  value       = aws_eks_cluster.main.name
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for the EKS cluster API server"
-  value       = aws_eks_cluster.main.endpoint
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_ca" {
   description = "Base64 encoded certificate authority data for the EKS cluster"
-  value       = aws_eks_cluster.main.certificate_authority[0].data
+  value       = module.eks.cluster_ca
 }
 
 output "oidc_provider_arn" {
   description = "ARN of the OIDC Provider for IRSA"
-  value       = aws_iam_openid_connect_provider.eks_oidc.arn
+  value       = module.eks.oidc_provider_arn
 }
 
 output "oidc_provider_url" {
   description = "URL of the OIDC Provider for IRSA"
-  value       = aws_iam_openid_connect_provider.eks_oidc.url
+  value       = module.eks.oidc_provider_url
 }
 
 output "node_group_arn" {
   description = "ARN of the EKS Node Group"
-  value       = aws_eks_node_group.main.arn
+  value       = module.eks.node_group_arn
 }
 
 output "cluster_security_group_id" {
   description = "Security group ID automatically created by EKS and attached to managed nodes"
-  value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  value       = module.eks.cluster_security_group_id
 }
 
 #output "to_configure_kubectl" {
