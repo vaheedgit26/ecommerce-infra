0e1c452fa886870169764a009a26e07b8864d028
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 20:57:31 +0530

    Update remote-state.tf

--- 20-rds/remote-state.tf
+++ 20-rds/remote-state.tf
@@ -5,7 +5,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket = var.remote_state_s3_bucket                                          # Name of the remote S3 bucket where the VPC state is stored
+    bucket = var.remote_state_s3_bucket                               # Name of the remote S3 bucket where the VPC state is stored
     key    = "${var.project}/${var.env}/vpc/terraform.tfstate"        # Path to the VPC tfstate file within the bucket
     region = var.region                                               # Region where the S3 bucket and DynamoDB table exist
   }
@@ -18,8 +18,8 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
 
   config = {
-    bucket = var.remote_state_s3_bucket                                          # Name of the remote S3 bucket where the VPC state is stored
-    key    = "${var.project}/${var.env}/eks/terraform.tfstate"        # Path to the VPC tfstate file within the bucket
+    bucket = var.remote_state_s3_bucket                               # Name of the remote S3 bucket where the EKS state is stored
+    key    = "${var.project}/${var.env}/eks/terraform.tfstate"        # Path to the EKS tfstate file within the bucket
     region = var.region                                               # Region where the S3 bucket and DynamoDB table exist
   }
 }
