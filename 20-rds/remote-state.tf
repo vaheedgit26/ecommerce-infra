fd48a8de70015eb3ed0ff198b29b1b549b0ca57d
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 21:02:44 +0530

    Update remote-state.tf

--- 20-rds/remote-state.tf
+++ 20-rds/remote-state.tf
@@ -25,6 +25,19 @@ data "terraform_remote_state" "eks" {
 }
 
 # --------------------------------------------------------------------
+# Reference the Remote State from BASTION Project
+# --------------------------------------------------------------------
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+
+  config = {
+    bucket = var.remote_state_s3_bucket                               # Name of the remote S3 bucket where the BASTION state is stored
+    key    = "${var.project}/${var.env}/bastion/terraform.tfstate"    # Path to the BASTION tfstate file within the bucket
+    region = var.region                                               # Region where the S3 bucket and DynamoDB table exist
+  }
+}
+
+# --------------------------------------------------------------------
 # Output the VPC ID from the remote VPC state
 # --------------------------------------------------------------------
 output "vpc_id" {
