842a317a13b44718213df85af4b1f981c1789f70
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 23:09:06 +0530

    Update variables.tf

--- 20-rds/variables.tf
+++ 20-rds/variables.tf
@@ -1,4 +1,4 @@
 variable "project" {}
 variable "env" {}
 
-variable "remote_state_s3_bucket" {}    # To read "vpc" outputs
+variable "remote_state_s3_bucket" {}    # To read "vpc, eks, bastion" outputs
