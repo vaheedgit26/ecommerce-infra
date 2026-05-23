b8b5e56145d55a958f0b533c96f70a7759343808
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 11:54:30 +0530

    Update main.tf

--- 40-secrets-manager/main.tf
+++ 40-secrets-manager/main.tf
@@ -3,7 +3,7 @@ module "secrets_manager" {
 
   project     = var.project          # "ecommerce"
   env         = var.env              # "dev"
-
+  secret_name = var.secret_name      # /ecommerce/dev/db-credentials
   db_username = var.db_username
   db_password = var.db_password
 }
