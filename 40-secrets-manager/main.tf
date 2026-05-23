142d144b647bbcd5125d2e413fdc3430c0e9a734
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 20:28:03 +0530

    Update main.tf

--- 40-secrets-manager/main.tf
+++ 40-secrets-manager/main.tf
@@ -1,9 +1,9 @@
 module "secrets_manager" {
   source = "git::https://github.com/vaheedgit26/Infra-1.0.git//modules/secrets-manager"
 
-  project     = var.project               # "ecommerce"
-  env         = var.env                   # "dev"
-  secret_name = local.db_secret_name      # /ecommerce/dev/db-credentials
+  project     = var.project                        # "ecommerce"
+  env         = var.env                            # "dev"
+  secret_name = local.postgre_db_secret_name       # /ecommerce/dev/postgre-db-credentials
   db_username = var.db_username
   db_password = var.db_password
 
