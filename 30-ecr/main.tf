f96564578616d4878d45b46a7be4ea4a0b6d8733
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 07:53:51 +0530

    Update main.tf

--- 30-ecr/main.tf
+++ 30-ecr/main.tf
@@ -1,13 +1,12 @@
 module "ecr" {
   source = "git::https://github.com/vaheedgit26/Infra-1.0.git//modules/ecr"
 
-  project = var.project  # "ecommerce"
-  env     = var.env      # "dev"
+  project = "ecommerce"
+  env     = "dev"
   repositories = [
-    "api-gateway",
-    "auth-service",
-    "pharma-ui",
-    "notification-service",
-    "catalog-service"
+    "ecommerce/product-service",
+    "ecommerce/cart-service",
+    "ecommerce/user-service",
+    "ecommerce/order-service"
   ]
 }
