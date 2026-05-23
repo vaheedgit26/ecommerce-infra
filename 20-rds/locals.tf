4831e7b9160140db5b6df60cdd8f769a4af6e39a
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 13:52:23 +0530

    Update locals.tf

--- 20-rds/locals.tf
+++ 20-rds/locals.tf
@@ -4,4 +4,11 @@ locals {
   identifier            = "${local.resource_name}-postgre"
   db_subnet_group_name  = "${local.resource_name}-postgre-rds-db-subnet-group"
   ecommerce_secret_json = jsondecode(data.aws_secretsmanager_secret_version.retailstore_secret_value.secret_string)
+
+  common_tags = {
+    Project     = var.project
+    Environment = var.env
+    Terraform   = "True"
+  }
+
 }
