a977218701f2d29ecae4b2e34a874d2180c60890
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 12:43:48 +0530

    Update main.tf

--- 20-rds/main.tf
+++ 20-rds/main.tf
@@ -2,7 +2,7 @@ module "rds" {
   source = "git::https://github.com/vaheedgit26/Infra-1.0.git//modules/rds"
   database_subnet_ids = data.terraform_remote_state.vpc.outputs.database_subnet_ids       # For subnet group creation
 
-  identifier              = "${var.project}-${var.env}-postgre"
+  identifier              = local.identifier
   availability_zone       = data.terraform_remote_state.vpc.outputs.availability_zones[0]         # var.availability_zone
   engine                  = "postgres"
   engine_version          = "15.7"
@@ -13,6 +13,6 @@ module "rds" {
   db_name                 = "ecommercedb"
   username                = local.ecommerce_secret_json.db_username
   password                = local.ecommerce_secret_json.db_password
-  db_subnet_group_name    = "${var.project}-${var.env}-postgre-rds-db-subnet-group" 
+  db_subnet_group_name    = local.db_subnet_group_name
   vpc_security_group_ids  = [data.terraform_remote_state.eks.outputs.cluster_security_group_id, data.terraform_remote_state.bastion.outputs.bastion_sg_id]            
 }
