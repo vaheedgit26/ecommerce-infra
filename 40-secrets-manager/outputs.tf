8cde79b5c63d0efbc03d63537d146d3df11555ac
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 20:18:44 +0530

    Update outputs.tf

--- 40-secrets-manager/outputs.tf
+++ 40-secrets-manager/outputs.tf
@@ -1,9 +1,9 @@
 output "db_secret_name" {
   description = "Name of the secret created"
-  value       = module.secrets_manager.db_credentials.name
+  value       = module.secrets_manager.db_secret_name
 }
 
 output "db_secret_arn" {
   description = "ARN of the secret created"
-  value       = module.secrets_manager.db_credentials.arn
+  value       = module.secrets_manager.db_secret_arn
 }
