4dfda99c1d1676529ecf780271db223ab1636d0d
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 14:16:31 +0530

    Update locals.tf

--- 20-rds/locals.tf
+++ 20-rds/locals.tf
@@ -1,6 +1,7 @@
 locals {
   resource_name = "${var.project}/${var.env}"
 
+  sg_name               = "${local.resource_name}-postgre-rds-sg"
   identifier            = "${local.resource_name}-postgre"
   db_subnet_group_name  = "${local.resource_name}-postgre-rds-db-subnet-group"
   ecommerce_secret_json = jsondecode(data.aws_secretsmanager_secret_version.retailstore_secret_value.secret_string)
