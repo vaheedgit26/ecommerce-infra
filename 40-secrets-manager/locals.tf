58084e25aa5775d8d5d26e37511d1cf3a9bf665c
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 13:11:39 +0530

    Update locals.tf

--- 40-secrets-manager/locals.tf
+++ 40-secrets-manager/locals.tf
@@ -1,3 +1,10 @@
 locals {
+
+  common_tags = {
+    Project     = var.project
+    Environment = var.env
+    Terraform   = "True"
+  }
+
   db_secret_name ="/${var.project}/${var.env}/db-credentials"
 }
