12260b4cb4853f8bc0a7a65b5ac92450b5cd2a5c
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 16:11:44 +0530

    Update locals.tf

--- 50-aws-lbc/locals.tf
+++ 50-aws-lbc/locals.tf
@@ -1,3 +1,6 @@
 locals {
   resource_name = "${var.project}-${var.env}"
+
+  eks_oidc_provider_url_ = data.terraform_remote_state.eks.outputs.oidc_provider_url
+  oidc_provider_arn = data.terraform_remote_state.eks.outputs.oidc_provider_arn
 }
