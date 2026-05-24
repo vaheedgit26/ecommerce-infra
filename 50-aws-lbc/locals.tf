77f48e07b024bb657e73a88c6e751bf56a0f9a37
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 16:23:01 +0530

    Update locals.tf

--- 50-aws-lbc/locals.tf
+++ 50-aws-lbc/locals.tf
@@ -4,7 +4,7 @@ locals {
   eks_oidc_provider_url = data.terraform_remote_state.eks.outputs.oidc_provider_url
   eks_oidc_provider_arn = data.terraform_remote_state.eks.outputs.oidc_provider_arn
 
-  eks_host = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  eks_cluster_ca_certificate = data.terraform_remote_state.eks.outputs.cluster_ca
+  eks_host  = data.terraform_remote_state.eks.outputs.cluster_endpoint
   eks_token = data.aws_eks_cluster_auth.cluster.token
+  eks_cluster_ca_certificate = data.terraform_remote_state.eks.outputs.cluster_ca
 }
