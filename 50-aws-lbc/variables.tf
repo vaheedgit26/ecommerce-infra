0872f8373c9762bf680a4e881c7715c420a0991e
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 16:14:17 +0530

    Update variables.tf

--- 50-aws-lbc/variables.tf
+++ 50-aws-lbc/variables.tf
@@ -1,7 +1,4 @@
 variable "project" {}
 variable "env" {}
 
-variable "oidc_provider_url" {}
-variable "oidc_provider_arn" {}
-
 variable "remote_state_s3_bucket" {}    # To read VPC and EKS output values
