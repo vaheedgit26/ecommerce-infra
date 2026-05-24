eb9767cb52a29fe6339e8401fc68a697e911ba9a
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 11:54:11 +0530

    Update 02-helm-kubernetes-providers.tf

--- 50-aws-lbc/02-helm-kubernetes-providers.tf
+++ 50-aws-lbc/02-helm-kubernetes-providers.tf
@@ -1,6 +1,6 @@
 # Datasource: EKS Cluster Auth 
 data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.main.id   # update form remote state
+  name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
 # HELM Provider
