2f0a21b1b6b2638fdbafd99b66d52da163ae904a
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 16:48:09 +0530

    Update 02-helm-kubernetes-providers.tf

--- 50-aws-lbc/02-helm-kubernetes-providers.tf
+++ 50-aws-lbc/02-helm-kubernetes-providers.tf
@@ -3,7 +3,7 @@ provider "helm" {
   kubernetes = {
     host                   = local.eks_host
     cluster_ca_certificate = base64decode(local.eks_cluster_ca_certificate)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    token                  = local.eks_token  # data.aws_eks_cluster_auth.cluster.token
   }
 }
 
@@ -11,5 +11,5 @@ provider "helm" {
 provider "kubernetes" {
   host                   = local.eks_host 
   cluster_ca_certificate = base64decode(local.eks_cluster_ca_certificate)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  token                  = local.eks_token    # data.aws_eks_cluster_auth.cluster.token
 }
