6c7421b39fa8c335ca928d113d50fcd01a9ae33f
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 19:44:28 +0530

    Update locals.tf

--- 70-eso/locals.tf
+++ 70-eso/locals.tf
@@ -1,6 +1,8 @@
 locals {
   resource_name = "${var.project}-${var.env}"
 
+  aws_account_id = data.aws_caller_identity.current.account_id
+
   eks_oidc_provider_url = data.terraform_remote_state.eks.outputs.oidc_provider_url
   eks_oidc_provider_arn = data.terraform_remote_state.eks.outputs.oidc_provider_arn
 
