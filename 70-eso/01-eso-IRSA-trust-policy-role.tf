1769b2667b51527e03d5626f5e99aa0ab3b0ef74
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 17:55:27 +0530

    Update 01-eso-IRSA-trust-policy-role.tf

--- 70-eso/01-eso-IRSA-trust-policy-role.tf
+++ 70-eso/01-eso-IRSA-trust-policy-role.tf
@@ -1,33 +1,33 @@
-data "aws_iam_policy_document" "eso_assume_role" {
+data "aws_iam_policy_document" "eso_trust_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(var.oidc_provider_url, "https://", "")}:sub"
+      variable = "${replace(local.eks_oidc_provider_url, "https://", "")}:sub"
       values   = ["system:serviceaccount:kube-system:external-secrets"]
     }
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(var.oidc_provider_url, "https://", "")}:aud"
+      variable = "${replace(local.eks_oidc_provider_url, "https://", "")}:aud"
       values   = ["sts.amazonaws.com"]
     }
 
     principals {
-      identifiers = [var.oidc_provider_arn]
+      identifiers = [local.eks_oidc_provider_arn]
       type        = "Federated"
     }
   }
 }
 
 resource "aws_iam_role" "eso_role" {
-  name               = "${var.project}-${var.env}-eso-role"
-  assume_role_policy = data.aws_iam_policy_document.eso_assume_role.json
+  name               = "${local.resource_name}-eso-role"
+  assume_role_policy = data.aws_iam_policy_document.eso_trust_policy.json
 
   tags = {
-    Name    = "${var.project}-${var.env}-eso-role"
+    Name    = "${local.resource_name}-eso-role"
     Env     = var.env
     Project = var.project
   }
