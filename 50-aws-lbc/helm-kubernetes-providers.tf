79f09fcc1d4fe4c6a80f37ea2d21b29b5f6cd2c7
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 10:58:09 +0530

    Update helm-kubernetes-providers.tf

--- 50-aws-lbc/helm-kubernetes-providers.tf
+++ 50-aws-lbc/helm-kubernetes-providers.tf
@@ -14,7 +14,7 @@ provider "helm" {
 
 # Terraform Kubernetes Provider
 provider "kubernetes" {
-  host = aws_eks_cluster.main.endpoint 
+  host                   = aws_eks_cluster.main.endpoint 
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  token = data.aws_eks_cluster_auth.cluster.token
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
