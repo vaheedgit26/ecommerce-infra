5b95496281c8637bc8d358b771e1dfd010fcb6c5
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 18:09:57 +0530

    Update data-remote-state.tf

--- 70-eso/data-remote-state.tf
+++ 70-eso/data-remote-state.tf
@@ -1,3 +1,6 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
 # --------------------------------------------------------------------
 # Reference the Remote State from EKS Project
 # --------------------------------------------------------------------
