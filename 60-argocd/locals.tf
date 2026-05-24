dfca4ce13aac3c608fa650e0090fd1f6bca84ee9
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 17:08:16 +0530

    Update locals.tf

--- 60-argocd/locals.tf
+++ 60-argocd/locals.tf
@@ -1,4 +1,6 @@
 locals {
-  eks_oidc_provider_url = 
-  eks_oidc_provider_arn = 
+  resource_name = "${var.project}-${var.env}"
+
+  eks_oidc_provider_url = data.terraform_remote_state.eks.outputs.oidc_provider_url
+  eks_oidc_provider_arn = data.terraform_remote_state.eks.outputs.oidc_provider_arn
 }
