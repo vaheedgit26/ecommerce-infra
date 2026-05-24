e87c7b906ad799b8cd17c33694dc1b25d71f781e
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 18:50:35 +0530

    Update 02-argocd-helm-install.tf

--- 60-argocd/02-argocd-helm-install.tf
+++ 60-argocd/02-argocd-helm-install.tf
@@ -1,3 +1,12 @@
+# HELM Provider
+provider "helm" {
+  kubernetes = {
+    host                   = local.eks_host
+    cluster_ca_certificate = base64decode(local.eks_cluster_ca_certificate)
+    token                  = local.eks_token  # data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
