826e08468b2676ecac99245415ead1031f98bc5f
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-24 10:12:33 +0530

    Update provider.tf

--- 50-aws-lbc/provider.tf
+++ 50-aws-lbc/provider.tf
@@ -5,11 +5,27 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 6.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.1.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.5.0"
+    }   
+
   }
 
 # Remote Backend
   backend "s3" {}      
-}
+}  # terraform end
 
 provider "aws" {
   region = var.region
