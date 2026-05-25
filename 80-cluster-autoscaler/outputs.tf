e78b63dbb7fa4c2c7e9ea8750e421cb0055d179e
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 12:50:02 +0530

    Update outputs.tf

--- 80-cluster-autoscaler/outputs.tf
+++ 80-cluster-autoscaler/outputs.tf
@@ -1,7 +1,10 @@
+output "eks_cluster_name" {
+  value = local.eks_cluster_name
+}
+
 output "eks_oidc_provider_url" {
-  value = data.terraform_remote_state.eks.outputs.oidc_provider_url
+  value = local.eks_oidc_provider_url
 }
 
 output "eks_oidc_provider_arn" {
-  value = data.terraform_remote_state.eks.outputs.oidc_provider_arn
-}
+  value = local.eks_oidc_provider_arn
