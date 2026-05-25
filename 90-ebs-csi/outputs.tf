9a8e4e7ad14a10479d16b56359d5d75216b2ade7
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 15:18:20 +0530

    Update outputs.tf

--- 90-ebs-csi/outputs.tf
+++ 90-ebs-csi/outputs.tf
@@ -1,3 +1,6 @@
+# -------------------------
+# EKS outputs
+# -------------------------
 output "eks_cluster_name" {
   value = local.eks_cluster_name
 }
@@ -12,4 +15,27 @@ output "eks_oidc_provider_url" {
 
 output "eks_oidc_provider_arn" {
   value = local.eks_oidc_provider_arn
+}
+
+# -------------------------
+# EBS CSI outputs
+# -------------------------
+output "ebs_csi_addon_default_version" {
+  description = "Default EBS CSI addon version compatible with the EKS cluster version"
+  value       = data.aws_eks_addon_version.ebs_csi_default.version
+}
+
+output "ebs_csi_addon_latest_version" {
+  description = "Latest available EBS CSI addon version for the current EKS cluster"
+  value       = data.aws_eks_addon_version.ebs_csi_latest.version
+}
+
+output "ebs_csi_addon_arn" {
+  description = "ARN of the installed EBS CSI addon"
+  value       = aws_eks_addon.ebs_csi.arn
+}
+
+output "ebs_csi_addon_id" {
+  description = "ID of the installed EBS CSI addon"
+  value       = aws_eks_addon.ebs_csi.id
 }
