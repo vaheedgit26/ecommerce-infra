27b5b82e47daaf1f89a4635d68585484d41623cd
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 18:47:50 +0530

    Update variables.tf

--- 70-eso/variables.tf
+++ 70-eso/variables.tf
@@ -2,4 +2,4 @@ variable "project" {}
 variable "env" {}
 variable "region" {}
 
-variable "remote_state_s3_bucket" {}    # To read VPC and EKS output values
+variable "remote_state_s3_bucket" {}    # To read EKS output values
