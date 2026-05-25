b22423c23715185c8964157c57da6d6a28488b0b
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 13:34:48 +0530

    Update outputs.tf

--- 60-argocd/outputs.tf
+++ 60-argocd/outputs.tf
@@ -1,3 +1,11 @@
+output "eks_cluster_name" {
+  value = local.eks_cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = local.eks_host
+}
+
 output "eks_oidc_provider_url" {
   value = data.terraform_remote_state.eks.outputs.oidc_provider_url
 }
