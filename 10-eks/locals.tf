7b426e2311d4b9c08400c8d7f926fae3fea3caef
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 09:02:21 +0530

    Update locals.tf

--- 10-eks/locals.tf
+++ 10-eks/locals.tf
@@ -1,5 +1,6 @@
 locals {
-   eks_cluster_name = data.terraform_remote_state.vpc.outputs.eks_cluster_name
+   eks_cluster_name = data.terraform_remote_state.vpc.outputs.eks_cluster_name  
+   eks_cluster_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
 
   node_auto_scaler_tags = {
     "k8s.io/cluster-autoscaler/enabled"                   = "true"
