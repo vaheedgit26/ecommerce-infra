2ec735ef40830ff8d52ae92d18c5680ac58cc208
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 18:24:37 +0530

    Update metrics-server-add-on.tf

--- 91-metrics-server/metrics-server-add-on.tf
+++ 91-metrics-server/metrics-server-add-on.tf
@@ -1,22 +1,30 @@
 # Datasource: To get default EKS addon version compatible with EKS cluster version
 data "aws_eks_addon_version" "metrics_server_default" {
   addon_name         = "metrics-server"
-  kubernetes_version = data.terraform_remote_state.eks.outputs.eks_cluster_version
+  kubernetes_version = local.eks_cluster_version
 }
 
 # Datasource: To get latest EKS addon version compatible with EKS cluster version
 data "aws_eks_addon_version" "metrics_server_latest" {
   addon_name         = "metrics-server"
-  kubernetes_version = data.terraform_remote_state.eks.outputs.eks_cluster_version
+  kubernetes_version = local.eks_cluster_version
   most_recent        = true
 }
 
-# EKS Addon: Pod Identity Agent
+# EKS Addon
 resource "aws_eks_addon" "metrics_server" {
-  cluster_name                = data.terraform_remote_state.eks.outputs.eks_cluster_id
+  cluster_name                = local.eks_cluster_name
   addon_name                  = "metrics-server"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
+
   # Use the latest EKS addon version compatible with the cluster's Kubernetes version
   addon_version               = data.aws_eks_addon_version.metrics_server_latest.version
+
+  tags = {
+    Name        = "${local.resource_name}-metrics-server"
+    Project     = var.project
+    Environment = var.env
+  }
+
 }
