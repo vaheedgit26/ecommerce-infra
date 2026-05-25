701a6675bf6ad3d0dc50926c8ab01e0cbc4bd049
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 09:31:18 +0530

    Update 02-eso-policy-attach.tf

--- 70-eso/02-eso-policy-attach.tf
+++ 70-eso/02-eso-policy-attach.tf
@@ -17,7 +17,7 @@ resource "aws_iam_policy" "eso_secrets_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eso_secrets_attachment" {
+resource "aws_iam_role_policy_attachment" "eso_secrets_policy_attachment" {
   role       = aws_iam_role.eso_role.name
   policy_arn = aws_iam_policy.eso_secrets_policy.arn
 }
