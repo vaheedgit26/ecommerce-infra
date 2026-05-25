b5a156ae60c1c567002c3c456dc32580279c7a6b
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 09:28:15 +0530

    Create 02-ca-policy-attach.tf

---
+++ 80-cluster-autoscaler/02-ca-policy-attach.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_policy" "cluster_autoscaler_policy" {
+  name        = "${local.resource_name}-cluster-autoscaler-policy"
+  description = "Allow cluster autoscaler to scaler nodes"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeScalingActivities",
+          "autoscaling:DescribeTags",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:DescribeImages",
+          "ec2:GetInstanceTypesFromInstanceRequirements",
+          "eks:DescribeNodegroup"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup"
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:ResourceTag/k8s.io/cluster-autoscaler/${local.eks_cluster_name}" = "owned"
+          }
+        }
+      }
+    ]
+  })
+}
