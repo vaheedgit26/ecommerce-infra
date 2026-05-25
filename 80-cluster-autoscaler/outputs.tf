7d4b2876a488fe43f55b3d96141dc593bb67176f
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-25 13:13:29 +0530

    Update outputs.tf

--- 80-cluster-autoscaler/outputs.tf
+++ 80-cluster-autoscaler/outputs.tf
@@ -2,6 +2,10 @@ output "eks_cluster_name" {
   value = local.eks_cluster_name
 }
 
+output "eks_cluster_endpoint" {
+  value = local.eks_host
+}
+
 output "eks_oidc_provider_url" {
   value = local.eks_oidc_provider_url
 }
