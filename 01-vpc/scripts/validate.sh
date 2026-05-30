f2efb5d9a304975fcc0b1b5da232ab87631447f6
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-30 21:54:55 +0530

    Update validate.sh

--- 01-vpc/scripts/validate.sh
+++ 01-vpc/scripts/validate.sh
@@ -1,72 +1,38 @@
 #!/usr/bin/env bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+# Validate component
+validate_component() {
+  local COMPONENT="$1"
+  local VALID_COMPONENT="$2"
+  
+  if [[ "$COMPONENT" != "$VALID_COMPONENT" ]]; then
+    return 1
+  fi
 
-print_error() {
-  echo -e "$R $1 $N" >&2  # send the error to error terminal STDERR
+  return 0
 }
 
-print_info() {
-  echo -e "$Y $1 $N"  
+validate_from_list() {
+  local value="$1"
+  shift                     # removes first argument
+  local valid_list=("$@")   # capture remaining args as array
+
+  for valid in "${valid_list[@]}"; do
+    if [[ "$value" == "$valid" ]]; then
+      return 0
+    fi
+  done
+
+  return 1
 }
 
-validate() {
-    
-  local COMPONENT=$1
-  local ENV=$2
-  local ACTION=$3
+# validate PROJECT, BUCKET and REGION
+validate_item() {
+  local ITEM="$1"
 
-  local PROJECT=$4
-  local BUCKET=$5
-  local REGION=$6
-  
-  # Validate component
-  if [[ "$COMPONENT" != "vpc" ]]; then
-    print_error "Component should be 'vpc'(lower case) only for VPC creation"
+  if [[ -z "$ITEM" ]]; then
     return 1
   fi
 
-  # Validate ENV
-  case "$ENV" in
-    dev|qa|prod) ;;
-    *)
-      print_error "❌ Invalid env: $ENV"
-      print_info  "Valid envs: dev|qa|prod"
-      return 1
-      ;;
-  esac
-
-  # Validate ACTION
-  case "$ACTION" in
-    plan|apply|destroy) ;;
-    *)
-      print_error "❌ Invalid action: $ACTION"
-      print_info  "Valid ACTIONS: plan|apply|destroy"
-      return 1
-      ;;
-  esac
-
-  # Validate PROJECT
-  if [[ -z "$PROJECT" ]]; then
-    print_error "❌ PROJECT not provided and not found in Terraform output"
-    return 1
-  fi
-
-  # Validate BUCKET
-  if [[ -z "$BUCKET" ]]; then
-    print_error "❌ BUCKET not provided and not found in Terraform output"
-    print_info "👉 Run bootstrap (00-s3) or pass BUCKET manually"
-    return 1
-  fi
-  
-  # Validate REGION
-  if [[ -z "$REGION" ]]; then
-    print_error "❌ REGION not provided and not found in Terraform output"
-    return 1
-  fi
-  
   return 0
 }
