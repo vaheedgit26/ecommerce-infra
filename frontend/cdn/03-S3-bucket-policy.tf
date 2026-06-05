68b14348009f8e3cba0e9ac5c662e6181ad12a14
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-05 19:53:45 +0530

    Create 03-S3-bucket-policy.tf

---
+++ frontend/cdn/03-S3-bucket-policy.tf
@@ -0,0 +1,28 @@
+# S3 BUCKET POLICY (ALLOW ONLY CLOUDFRONT)
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+
+        Action = "s3:GetObject"
+
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
