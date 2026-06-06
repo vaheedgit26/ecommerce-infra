ef22dae76eb1365da106c2f977efb4848d85344b
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-06 20:31:03 +0530

    Update 02-app-pod-policy-attach.tf

--- MISC/app-sa/02-app-pod-policy-attach.tf
+++ MISC/app-sa/02-app-pod-policy-attach.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_policy" "eso_secrets_policy" {
+resource "aws_iam_policy" "app_pod_secrets_policy" {
   name        = "${local.resource_name}-eso-secrets-policy"
-  description = "Allow External Secrets Operator to read /${var.project}/${var.env} secrets from AWS Secrets Manager"
+  description = "Allow app pod to read /${var.project}/${var.env} secrets from AWS Secrets Manager"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -24,14 +24,14 @@ resource "aws_iam_policy" "eso_secrets_policy" {
           "ssm:GetParameters",
           "ssm:GetParametersByPath"
         ]
-        Resource = "arn:aws:ssm:*:${local.aws_account_id}:parameter/${var.project}/${var.env}/*"
-        # Resource = "arn:aws:ssm:${var.region}:${local.aws_account_id}:parameter/${var.project}/${var.env}/*"
+        Resource = "arn:aws:ssm:*:${local.aws_account_id}:parameter:/${var.project}/${var.env}/*"
+        # Resource = "arn:aws:ssm:${var.region}:${local.aws_account_id}:parameter:/${var.project}/${var.env}/*"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eso_secrets_policy_attachment" {
-  role       = aws_iam_role.eso_role.name
-  policy_arn = aws_iam_policy.eso_secrets_policy.arn
+resource "aws_iam_role_policy_attachment" "app_pod_secrets_policy_attachment" {
+  role       = aws_iam_role.app_pod_role.name
+  policy_arn = aws_iam_policy.app_pod_secrets_policy.arn
 }
