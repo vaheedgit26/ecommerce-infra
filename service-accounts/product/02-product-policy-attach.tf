5e0ffbb33515361d1fea55791a17742b6bdfd865
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-06 21:50:56 +0530

    Create 02-product-policy-attach.tf

---
+++ service-accounts/product/02-product-policy-attach.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_policy" "product_dynamodb_policy"" {
+  name = "${var.project}-${var.env}-dynamodb-access"
+  description = "Allow product pod to read /${var.project}/${var.env} dynamo db table"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = "arn:aws:dynamodb:${var.region}:${local.aws_account_id}:table/${var.project}-${var.env}-products"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eso_secrets_policy_attachment" {
+  role       = aws_iam_role.eso_role.name
+  policy_arn = aws_iam_policy.eso_secrets_policy.arn
+}
