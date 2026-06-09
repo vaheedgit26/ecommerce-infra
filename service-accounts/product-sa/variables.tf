87d4c1696850a50fe76c763fe7ef8a770fc690d4
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-09 09:57:12 +0530

    Update variables.tf

--- service-accounts/product-sa/variables.tf
+++ service-accounts/product-sa/variables.tf
@@ -2,6 +2,6 @@ variable "project" {}
 variable "env" {}
 variable "region" {}
 
-variable "namespace" {}
+variable "namespace" { "ecommerce-dev" }
 variable "service_account" { default = "product" }
 variable "dynamodb_table" { default = "ecommerce-products" }
